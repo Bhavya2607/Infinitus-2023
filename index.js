6314e7a6b94f1bf7e29bc08f35d5aa47bde39f1c
Bhavya2607 <86648149+Bhavya2607@users.noreply.github.com>
2023-08-15 17:57:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ gsap.from("#main h2 img", {
         scrub: 0,
     }
 });
+
+document.addEventListener("mousemove", function(details){
+    console.log(details)
+    document.querySelectorAll(".img").forEach((elem) =>{
+        const position = elem.getAttribute("value");
+        var x = (window.innerWidth - details.clientX * position)/50;
+        var y = (window.innerHeight - details.clientY * position)/50;
+        elem.style.transform = `translateX(${x}px) translateY(${y}px) translate(-50%, 0%) rotate(-2deg) `
+    })
+})
